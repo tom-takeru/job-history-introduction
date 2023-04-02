2ea2a92a0ec724939ec607162f37a654ffc8ca0c
tom-takeru <tom.2461.ty@gmail.com>
2023-04-03 01:43:27 +0900

    creat template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SideMenuComponent } from '@/components/SideMenuComponent';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='w-full min-h-screen flex'>
+      <div className='z-10 w-full min-h-full flex'>
+        <SideMenuComponent />
+        <div className='w-4/5 min-h-full p-8'>
+          <Component {...pageProps} />
+        </div>
+      </div>
+      <div
+        className='fixed top-0 left-0 right-0 bottom-0 z-0 bg-cover bg-no-repeat bg-center opacity-10'
+        style={{ backgroundImage: "url('https://picsum.photos/1920/1080')" }}
+      ></div>
+    </div>
+  );
 }
